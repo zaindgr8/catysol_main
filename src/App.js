942b47ab9199df1b05868c1cc1e2e095433f1f36
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-04-18 12:30:30 +0400

    1230

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import TokenAllocation from './pages/TokenAllocation/TokenAllocation';
 import Roadmap from './pages/TokenAllocation/Roadmap';
 import Faq from './pages/TokenAllocation/Faq';
 import About from './components/About';
-
-
+import Whitepaper from "./pages/TokenAllocation/Whitepaper"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/token-allocation' element={<TokenAllocation/>}/>
-        <Route exact path='/roadmap' element={<Roadmap/>}/>
-        <Route exact path='/faq' element={<Faq/>}/>
-        <Route exact path='/about' element={<About/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/token-allocation" element={<TokenAllocation />} />
+        <Route exact path="/roadmap" element={<Roadmap />} />
+        <Route exact path="/faq" element={<Faq />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/whitepaper" element={<Whitepaper />} />
       </Routes>
     </div>
   );
---
+++ src/pages/TokenAllocation/Whitepaper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Whitepaper = () => {
+  return (
+    <div>
+        Whitepaper
+    </div>
+  )
+}
+
+export default Whitepaper
